8d983b1d7fb5dbf63a421814fd6000918182e5eb
Darin Mateev <mateevd@gmail.com>
2019-07-29 11:45:54 +0200

    fix absences

--- resources/views/absences/absence_modals.blade.php
+++ resources/views/absences/absence_modals.blade.php
@@ -44,7 +44,7 @@
 				<div class="modal-custom-libelle">Absence</div>
 
 				<form id="absence_update" action="{{route('absences.update', $userAbsence->absence_id)}}" method="post"
-					  class="needs-validation hide-submit modal-custom-form" novalidate>
+				      class="needs-validation hide-submit modal-custom-form" novalidate>
 					{{method_field('put')}}
 					{{csrf_field()}}
 
@@ -53,7 +53,7 @@
 					<div class="flex-row flex-wrap-yes justify-center items-center pad-v-small pad-h-medium bg-important-back width-100 ln-b-dashed-small">
 						{{ Form::label("absence_date", trans('absences.lab_absence_date'),["class"=>"style-important control-label pad-h-medium"]) }}
 						<span class="pad-h-medium">
-							{{ Form::date("absence_date", \Carbon\Carbon::parse($current_date)->startOfMonth(),["class"=>"form-control modal-custom-fields-input style-important", 'placeholder'=>trans('absences.lab_total_days'), 'required', 'tabindex'=>'1']) }}
+							{{ Form::date("absence_date", \Carbon\Carbon::parse($current_date)->startOfMonth(),["class"=>"form-control m-field-input style-important", 'placeholder'=>trans('absences.lab_total_days'), 'required', 'tabindex'=>'1']) }}
 							<div class="invalid-feedback show-warning-small">{{trans('app.mandatory_date')}}</div>
 							<div class="invalid-feedback show-warning-small pad-v-top-medium"></div>
 						</span>
@@ -64,13 +64,13 @@
 
 					<div class="flex-row flex-wrap-yes justify-evenly pad-h-medium pad-v-small pad-v-bottom-medium ln-b-solid-small width-100">
 						<div class="flex-col flex-wrap-no justify-flex-end text-center pad-h-only-small">
-							{{ Form::label("absence_type_id", trans('absences.lab_type'),["class"=>"dashboard-label bold"]) }}
-							{{ Form::select("absence_type_id", $absence_types, null,["class"=>"form-control modal-custom-fields-input", 'tabindex'=>'2']) }}
+							{{ Form::label("absence_type_id", trans('absences.lab_type'),["class"=>"form-label"]) }}
+							{{ Form::select("absence_type_id", $absence_types, null,["class"=>"form-control m-field-input", 'tabindex'=>'2']) }}
 						</div>
 					</div>
 
 					<div class="flex-row flex-wrap-yes pad-h-medium pad-v-small justify-center items-center ln-t-solid-small width-100">
-						<label for="hours_history" class="dashboard-label text-center bold">Ajouter / Ôter des jours</label>
+						<label for="hours_history" class="form-label">Ajouter / Ôter des jours</label>
 					</div>
 
 					<div class="flex-row flex-wrap-yes pad-h-medium pad-v-small justify-center items-center">
@@ -79,34 +79,34 @@
 							<div class="flex-row flex-wrap-yes justify-evenly pad-h-medium pad-v-small" id="hoursBtns">
 								<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small">
 									<button type="button" value="0.5" tabindex="10"
-											class="modal-custom-btn-absence modal-custom-btn-absence-add"
-											data-toggle="tooltip"
-											data-placement="bottom"
-											title="Ajouter 0,5 jour">+0,5 j
+									        class="modal-custom-btn-absence modal-custom-btn-absence-add"
+									        data-toggle="tooltip"
+									        data-placement="bottom"
+									        title="Ajouter 0,5 jour">+0,5 j
 									</button>
 								</div>
 								<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small">
 									<button type="button" value="1" tabindex="11"
-											class="modal-custom-btn-absence modal-custom-btn-absence-add"
-											data-toggle="tooltip"
-											data-placement="bottom"
-											title="Ajouter 1 jour">+1 j
+									        class="modal-custom-btn-absence modal-custom-btn-absence-add"
+									        data-toggle="tooltip"
+									        data-placement="bottom"
+									        title="Ajouter 1 jour">+1 j
 									</button>
 								</div>
 								<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small">
 									<button type="button" value="2" tabindex="12"
-											class="modal-custom-btn-absence modal-custom-btn-absence-add"
-											data-toggle="tooltip"
-											data-placement="bottom"
-											title="Ajouter 2 jours">+2 j
+									        class="modal-custom-btn-absence modal-custom-btn-absence-add"
+									        data-toggle="tooltip"
+									        data-placement="bottom"
+									        title="Ajouter 2 jours">+2 j
 									</button>
 								</div>
 								<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small">
 									<button type="button" value="5" tabindex="13"
-											class="modal-custom-btn-absence modal-custom-btn-absence-add"
-											data-toggle="tooltip"
-											data-placement="bottom"
-											title="Ajouter 5 jours">+5 j
+									        class="modal-custom-btn-absence modal-custom-btn-absence-add"
+									        data-toggle="tooltip"
+									        data-placement="bottom"
+									        title="Ajouter 5 jours">+5 j
 									</button>
 								</div>
 							</div>
@@ -119,34 +119,34 @@
 							<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small" id="hoursBtns">
 								<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small">
 									<button type="button" value="-0.5" tabindex="20"
-											class="modal-custom-btn-absence modal-custom-btn-absence-doff"
-											data-toggle="tooltip"
-											data-placement="bottom"
-											title="Ôter 0,5 jour">-0,5 j
+									        class="modal-custom-btn-absence modal-custom-btn-absence-doff"
+									        data-toggle="tooltip"
+									        data-placement="bottom"
+									        title="Ôter 0,5 jour">-0,5 j
 									</button>
 								</div>
 								<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small">
 									<button type="button" value="-1" tabindex="21"
-											class="modal-custom-btn-absence modal-custom-btn-absence-doff"
-											data-toggle="tooltip"
-											data-placement="bottom"
-											title="Ôter 1 jour">-1 j
+									        class="modal-custom-btn-absence modal-custom-btn-absence-doff"
+									        data-toggle="tooltip"
+									        data-placement="bottom"
+									        title="Ôter 1 jour">-1 j
 									</button>
 								</div>
 								<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small">
 									<button type="button" value="-2" tabindex="22"
-											class="modal-custom-btn-absence modal-custom-btn-absence-doff"
-											data-toggle="tooltip"
-											data-placement="bottom"
-											title="Ôter 2 jours">-2 j
+									        class="modal-custom-btn-absence modal-custom-btn-absence-doff"
+									        data-toggle="tooltip"
+									        data-placement="bottom"
+									        title="Ôter 2 jours">-2 j
 									</button>
 								</div>
 								<div class="flex-row flex-wrap-yes justify-evenly pad-v-small pad-h-only-small">
 									<button type="button" value="-5" tabindex="23"
-											class="modal-custom-btn-absence modal-custom-btn-absence-doff"
-											data-toggle="tooltip"
-											data-placement="bottom"
-											title="Ôter 5 jours">-5 j
+									        class="modal-custom-btn-absence modal-custom-btn-absence-doff"
+									        data-toggle="tooltip"
+									        data-placement="bottom"
+									        title="Ôter 5 jours">-5 j
 									</button>
 								</div>
 							</div>
@@ -157,7 +157,7 @@
 						<div class="flex-row flex-wrap-yes justify-evenly pad-h-medium pad-v-small flex-wrap-no justify-center align-center" id="hoursBtns">
 							<label for="absence_days" class="control-label" hidden></label>
 							<input type="number" id="absence_days" name="absence_days"
-							       class="form-control modal-custom-fields-input input-hours-absences" count="0"
+							       class="form-control m-field-input input-hours-absences" count="0"
 							       value="0"
 							       step="any" placeholder="{{trans('absences.lab_total_days')}}" required='required'
 							       disabled>
@@ -172,9 +172,9 @@
 					</div>
 
 					<div class="flex-row flex-wrap-yes justify-evenly pad-h-medium pad-v-small width-100">
-						<label for="absence_description" class="dashboard-label bold">{{ucfirst(trans('app.description'))}}</label>
-						<input type="textarea" class="form-control modal-custom-area-input" name="absence_description"
-								id="absence_description" value="" tabindex="40">
+						<label for="absence_description" class="form-label">{{ucfirst(trans('app.description'))}}</label>
+						<input type="textarea" class="form-control m-area-input" name="absence_description"
+						       id="absence_description" value="" tabindex="40">
 					</div>
 
 					<div class="modal-custom-detail style-ids" id="absence_id"></div>
